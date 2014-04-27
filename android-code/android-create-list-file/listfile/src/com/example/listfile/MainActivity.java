3cc31450e5df8e56f33f8317777c3b39d30f26e3
baichuan <sccxboy@gmail.com>
2014-04-28 00:59:11 +0800

    simple test for create file and list file

--- android-code/android-create-list-file/listfile/src/com/example/listfile/MainActivity.java
+++ android-code/android-create-list-file/listfile/src/com/example/listfile/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.listfile;
 
 import java.io.File;   
 import java.io.IOException;   
-
 import android.app.Activity;   
 import android.widget.TextView;
 import android.os.Bundle;      
@@ -16,22 +15,99 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         int myProcessID = Process.myPid();
-        File yygypath = this.getFilesDir();//this.getCacheDir();
-        String yygypathstr = yygypath.toString();
+        int port = 5555;
+        String listresult = "", result = "";
         
-        /***************************************************************
-         * filename head:
-         * get rsl: 61CC7AE0
-         * hpid: C966A01E
-         * uhpid: 50D55DB7
-         * ht4port: 4A6B2318
-         * uht4port: 7FAFD9B1
+        //get rsl
+        result = createListfile(1, 0);
+        listresult = listresult + result + "  ";
+        try {
+        	Thread.sleep(10000);
+        } catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+        	e.printStackTrace();
+        }
+        result = createListfile(2, myProcessID);
+        listresult = listresult + " hpid result " + result;
+        try {
+        	Thread.sleep(10000);
+        } catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+        	e.printStackTrace();
+        }
+        result = createListfile(3, myProcessID);
+        listresult = listresult + " unhpid result " + result;
+        try {
+        	Thread.sleep(10000);
+        } catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+        	e.printStackTrace();
+        }
+        result = createListfile(4, port);
+        listresult = listresult + " hport result " + result;
+        try {
+        	Thread.sleep(10000);
+        } catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+        	e.printStackTrace();
+        }
+        result = createListfile(5, port);
+        listresult = listresult + " unhport result " + result;
+        try {
+        	Thread.sleep(10000);
+        } catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+        	e.printStackTrace();
+        }
+        
+        TextView  tv = new TextView(this);
+        tv.setText(listresult);
+        setContentView(tv);
+    }
+    
+    private String createListfile(int mode, int param)
+    {
+    	/***************************************************************
+         *info       filename head:   mode:        
+         * get rsl:    	61CC7AE0        1
+         * hpid: 		C966A01E        2
+         * uhpid: 		50D55DB7        3
+         * ht4port: 	4A6B2318        4
+         * uht4port: 	7FAFD9B1        5
          * ************************************************************/
-        String name = "61CC7AE0";
-        //String name = "C966A01E";
-        name = name + myProcessID;
-        
-        File file = new File(yygypath, name); 
+    	File filesPath = this.getFilesDir();//this.getCacheDir();
+    	String filesPathStr = filesPath.toString();
+    	String name;
+    	switch(mode)
+    	{
+    	case 1:
+    		//get rsl
+            name = "61CC7AE0";
+            break;
+    	case 2:
+    		//hpid
+    		name = "C966A01E";
+    		name = name + param;
+    		break;
+    	case 3:
+    		//uhpid
+    		name = "50D55DB7";
+    		name = name + param;
+    		break;
+    	case 4:
+    		//ht4port
+    		name = "4A6B2318";
+    		name = name + param;
+    		break;
+    	case 5:
+    		//uht4port
+    		name = "7FAFD9B1";
+    		name = name + param;
+    		break;
+    	default:
+    		return null;
+    	}
+    	File file = new File(filesPath, name); 
         try {
 			file.createNewFile();
 		} catch (IOException e) {
@@ -39,7 +115,7 @@ public class MainActivity extends Activity
 			e.printStackTrace();
 		}
         String result = ""; 
-        File file11 = new File(yygypathstr);
+        File file11 = new File(filesPathStr);
         File[] files = file11.listFiles(); 
         for (File fike : files) 
         { 
@@ -47,16 +123,23 @@ public class MainActivity extends Activity
         } 
         if (result.equals(""))
         {
-        	result = "找不到文件!!"; 
+        	result = "don't find file!!!!"; 
         }
-       String listresult = result;
-      
-       yygypathstr = yygypathstr + "  " + listresult + " pid is " + myProcessID;
-       file.delete();
-       TextView  tv = new TextView(this);
-       tv.setText(yygypathstr);
-       setContentView(tv);
+        file.delete(); 
+
+		return result;
     }
+    /*
+    private String codePid(int pid)
+    {
+    	if(pid < 0 || pid > 65535)
+    	{
+    		return  null;
+    	}
+    	//0-9  A-Z  total 36 char
+    	String result = "";
+    	int pid1, pid2, pid3, pid4;    	
+    }*/
     public void onDestory()
     {
     	super.onDestroy();
