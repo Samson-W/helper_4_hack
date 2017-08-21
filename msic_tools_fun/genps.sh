f1f9abeed6acef578952c7210f4b7f4eb2ad74d0
samson <sccxboy@gmail.com>
2017-08-21 16:24:13 +0800

    Add empty str check method.

--- msic_tools_fun/genps.sh
+++ msic_tools_fun/genps.sh
@@ -3,13 +3,21 @@
 # usage: 
 # bash ./genps.sh psstr
 
-PSSTR=$1
+USAGEHELP="usage: bash ./genps.sh psstr"
+
+PSSTR="$1"
 LENGTH=$(echo "${PSSTR}" | wc -c)
 ((CUTEND=$LENGTH+15))
 
-if [ ${CUTEND} -gt 512 ];then
-	echo "length is too long."
+if [ -z "${PSSTR}" ];then
+	echo "str is empty."
+	echo ${USAGEHELP}
 	exit 1
+	if [ ${CUTEND} -gt 512 ];then
+		echo "length is too long."
+		echo ${USAGEHELP}
+		exit 1
+	fi
 fi
 
 PSSTRNOW=$(echo "${PSSTR}" | openssl sha512 | openssl base64 | openssl sha512 | openssl base64 | openssl sha512 | awk '{printf $2}' | cut -c${LENGTH}-${CUTEND})
